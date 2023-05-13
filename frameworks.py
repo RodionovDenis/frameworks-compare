780ebd94d348ab27026d4f175ea065b3964182a7
RodionovDenis <denisrodionov18@gmail.com>
2023-05-14 02:18:21 +0300

    optuna impl

--- frameworks.py
+++ frameworks.py
@@ -1,7 +1,10 @@
-from abc import ABC, abstractclassmethod
-from dataclasses import dataclass
+import optuna
+import numpy.typing as npt
 
-from .metrics import Metric
+from abc import ABC, abstractclassmethod
+from dataclasses import dataclass, astuple
+
+from metrics import Metric
 from data.loader import Dataset
 
 @dataclass
@@ -13,23 +16,50 @@ class Hyperparameter:
 
 
 class Searcher(ABC):
-    def __init__(self, estimator, hyperparams: list[Hyperparameter], max_iter: int):
-        self.estimator = estimator
-        self.hyperparams = hyperparams
+    def __init__(self, max_iter: int):
         self.max_iter = max_iter
 
     @abstractclassmethod
-    def tune(inputs, targets, metric: Metric):
-        pass
-
-    @abstractclassmethod
-    def __str__():
+    def tune(self,
+             estimator, 
+             hyperparams: list[Hyperparameter],
+             features: npt.NDArray, 
+             targets, 
+             metric: Metric):
         pass
 
 
 class OptunaSearcher(Searcher):
-    pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.name = 'Optuna'
 
+    def tune(self,
+             estimator, 
+             hyperparams: list[Hyperparameter],
+             features: npt.NDArray, 
+             targets, 
+             metric: Metric):
+
+        self.estimator, self.hyperparams, self.features, self.targets, self.metric = \
+            estimator, hyperparams, features, targets, metric
+        optuna.logging.disable_default_handler()
+        study = optuna.create_study(direction='maximize')
+        study.optimize(self.__objective, n_trials=self.max_iter)
+        return study.best_value
+
+    def __objective(self, trial: optuna.Trial):
+        arguments = {}
+        for params in self.hyperparams:
+            name, group, min_value, max_value = astuple(params)
+            if group is float:
+                arguments[name] = trial.suggest_float(name, min_value, max_value)
+            elif group is int:
+                arguments[name] = trial.suggest_int(name, min_value, max_value)
+        model = self.estimator(**arguments)
+        return self.metric(model, self.features, self.targets)
+
+    
 
 class HyperoptSearcher(Searcher):
     pass
@@ -37,11 +67,3 @@ class HyperoptSearcher(Searcher):
 
 class iOptSearcher(Searcher):
     pass
-
-
-def run_searchers(searchers: list[Searcher], datasets: list[Dataset]):
-    pass
-
-
-
-
