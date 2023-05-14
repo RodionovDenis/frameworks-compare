dd247d733c01c2ceebdcf43a14ed41e0e7a0181e
RodionovDenis <denisrodionov18@gmail.com>
2023-05-14 23:54:02 +0300

    add saving

--- experiment.py
+++ experiment.py
@@ -1,11 +1,15 @@
 import pandas as pd
+import json
 
-from frameworks import Searcher, Hyperparameter
+from frameworks import Searcher, Hyperparameter, dict_factory
 from data.loader import Parser, get_datasets
 from metrics import Metric
+
+from pathlib import Path
 from tqdm import tqdm
 from itertools import product
 from functools import partial
+from dataclasses import asdict
 
 class Experiment:
     def __init__(self, estimator,
@@ -13,6 +17,7 @@ class Experiment:
                        searchers: list[Searcher],
                        parsers: list[Parser],
                        metric: Metric):
+
         self.estimator = estimator
         self.searcher_classes = searchers
         self.datasets = get_datasets(*parsers)
@@ -26,7 +31,7 @@ class Experiment:
         self.seacher_instances = [searcher(**kwargs) for searcher in self.searcher_classes]
         self.max_iter = kwargs['max_iter']
 
-    def run(self, default_arguments=False):
+    def run(self, default_arguments=False, show_result=False, path_to_folder=None):
 
         assert 'seacher_instances' in self.__dict__, \
             'First you need to set the parameters using the set_framework_arguments method'
@@ -48,8 +53,27 @@ class Experiment:
             if default_arguments:
                 self.__calculate_default_arguments(frame, metric)
 
+        if show_result:
+            print(frame)
+        
+        if isinstance(path_to_folder, str):
+            self.__save(frame, path_to_folder)
+
         return frame
     
     def __calculate_default_arguments(self, frame, metric: Metric):
         for dataset in self.datasets:
             frame[self.default_arguments][dataset.name] = metric(self.estimator(), dataset)
+
+    def __save(self, frame, path_to_folder: str):
+        metainfo = {
+            'estimator': self.estimator.__name__,
+            'max_iter': self.max_iter,
+            'hyperparameters': [asdict(x, dict_factory=dict_factory) for x in  self.hyperparams],
+            'metric': self.metric.name
+        }
+        path = Path(path_to_folder)
+        path.mkdir(parents=True, exist_ok=True)
+        with open(path / 'metainfo.json', 'w') as f:
+            f.write(json.dumps(metainfo, indent=4))
+        frame.to_csv(path / 'scores.svc')
