d1057ac9e5502226b32790410e2b62024383f2ad
RodionovDenis <denisrodionov18@gmail.com>
2023-05-15 18:18:13 +0300

    fix experiment interface

--- experiments/svc.py
+++ experiments/svc.py
@@ -8,17 +8,18 @@ from sklearn.svm import SVC
 
 
 if __name__ == '__main__':
+    
     hyperparams = [
-        Hyperparameter('gamma', Type.float, 1e-5, 1e-2),
-        Hyperparameter('C', Type.int, 1, 200)
+        Hyperparameter('gamma', Type.float(1e-3, 1e-1, log=True)),
+        Hyperparameter('C', Type.int(1, 1e6, log=True))
     ]
 
-    seachers = [HyperoptSearcher, OptunaSearcher, iOptSearcher]
+    seachers = [OptunaSearcher, HyperoptSearcher, iOptSearcher]
     parsers = [CNAE9, BreastCancer]
 
     experiment = Experiment(SVC, hyperparams, seachers, parsers,
                             CrossValidation(f1_score, average='weighted'))
     
-    experiment.set_framework_arguments(max_iter=5)
+    experiment.set_framework_arguments(max_iter=10)
 
-    experiment.run(default_arguments=True, show_result=True, path_to_folder='result/svc')
+    experiment.run(default_arguments=True, show_result=True, n_jobs=8, path_to_folder='result/svc')
