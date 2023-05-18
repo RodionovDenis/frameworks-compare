894356433b5063f95a2c303930f6fae002fc8c99
RodionovDenis <denisrodionov18@gmail.com>
2023-05-18 15:19:08 +0300

    change interface

--- experiment.py
+++ experiment.py
@@ -11,62 +11,71 @@ from tqdm import tqdm
 from itertools import product
 from dataclasses import asdict
 
+
 class Experiment:
     def __init__(self, estimator,
                        hyperparams: list[Hyperparameter],
                        searchers: list[Searcher],
                        parsers: list[Parser],
-                       metric: Metric):
+                       metric: Metric,
+                       *,
+                       path_to_folder=None,
+                       n_jobs=1,
+                       ):
 
         self.estimator = estimator
-        self.searcher_classes = searchers
+        self.searchers = searchers
         self.datasets = get_datasets(*parsers)
         self.hyperparams = hyperparams
         self.metric = metric
+
+        self.path_to_folder = path_to_folder
+        self.n_jobs = n_jobs
+        
         self.default_arguments = 'Default Arguments'
 
-    
-    def set_framework_arguments(self, **kwargs):
-        assert 'max_iter' in kwargs, 'max_iter parameter must have'
-        self.max_iter = kwargs['max_iter']
-        self.seacher_instances = [searcher(**kwargs) for searcher in self.searcher_classes]
+    def run(self, max_iter, *, default_arguments=False, show_result=False):
+        
+        self.searcher_instances = self.__initialized_searchers(max_iter=max_iter)
 
-    def run(self, default_arguments=False, show_result=False, path_to_folder=None, n_jobs=1):
-
-        assert 'seacher_instances' in self.__dict__, \
-            'First you need to set the parameters using the set_framework_arguments method'
-
-        columns = [self.default_arguments] * default_arguments + [x.name for x in self.seacher_instances]
-        frame = pd.DataFrame(index=[x.name for x in self.datasets], columns=columns)
-
+        columns = [self.default_arguments] * default_arguments + [x.name for x in self.searcher_instances]
+        indexes = [x.name for x in self.datasets]
+        frame = pd.DataFrame(index=indexes, columns=columns)
+        
         len_searchers, len_dataset = len(columns), len(self.datasets)
-
         products = list(product(range(len_searchers), range(len_dataset)))
 
-        with Pool(n_jobs) as pool:
+        with Pool(self.n_jobs) as pool:
             results = list(tqdm(pool.imap(self.objective, products), total=len(products)))
             for (i, j), value in zip(products, results):
-                column = self.seacher_instances[i].name if i < len(self.seacher_instances) else self.default_arguments
+                if i < len(self.searcher_instances):
+                    column = self.searcher_instances[i].name 
+                else:
+                    column = self.default_arguments
+
                 row = self.datasets[j].name
                 frame[column][row] = value
 
         if show_result:
             print(frame)
         
-        if isinstance(path_to_folder, str):
-            self.__save(frame, path_to_folder)
+        if isinstance(self.path_to_folder, str):
+            self.__save(frame, self.path_to_folder)
 
         return frame
     
-    def objective(self, args):
-        dataset = self.datasets[args[1]]
-        if args[0] == len(self.searcher_classes):
-            return self.__default_arguments(dataset)
-        searcher = self.seacher_instances[args[0]]
-        return searcher.tune(self.estimator, self.hyperparams, dataset, self.metric)
+    def __initialized_searchers(self, *args, **kwargs):
+        return [searcher(*args, **kwargs) for searcher in self.searchers]
     
-    def __default_arguments(self, dataset):
-        return self.metric(self.estimator(), dataset)
+    def objective(self, args):
+        idx_searcher, idx_dataset = args
+        dataset = self.datasets[idx_dataset]
+
+        if idx_searcher == len(self.searchers):
+            return self.metric(self.estimator(), dataset)
+
+        searcher = self.searcher_instances[idx_searcher]
+        return searcher.tune(self.estimator, self.hyperparams, dataset, self.metric)
 
     def __save(self, frame, path_to_folder: str):
         metainfo = {
