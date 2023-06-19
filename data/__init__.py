273518e94f902fa413cfc68bc3290ea4baeb2f99
RodionovDenis <denisrodionov18@gmail.com>
2023-06-19 14:18:26 +0300

    init in data

---
+++ data/__init__.py
@@ -0,0 +1,14 @@
+from .loader import (Adult, 
+                     BreastCancer, 
+                     Digits, 
+                     BankMarketing, 
+                     CNAE9, 
+                     StatlogSegmentation, 
+                     DryBean, 
+                     MagicGammaTelescope,
+                     Mushroom,
+                     Semeion,
+                     WineQuality)
+
+__all__ = [Adult, BreastCancer, Digits, BankMarketing, CNAE9, StatlogSegmentation, DryBean, MagicGammaTelescope,
+           Mushroom, Semeion, WineQuality]
