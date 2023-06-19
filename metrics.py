adfae12cf0f1b9361edd19d23c534fbfbef8a7df
RodionovDenis <denisrodionov18@gmail.com>
2023-06-19 14:18:03 +0300

    preprocessing as parameter

--- metrics.py
+++ metrics.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractclassmethod
 from typing import Literal
 
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, StratifiedKFold
 from sklearn.metrics import (
     accuracy_score,
     f1_score,
@@ -9,22 +9,39 @@ from sklearn.metrics import (
     mean_squared_error
 )
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
-
-from functools import partial
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, MaxAbsScaler
 from data.loader import Dataset
 
 
+PREPROCESSING = {
+    'standard': StandardScaler,
+    'min_max': MinMaxScaler,
+    'max_abs': MaxAbsScaler,
+}
+
+
 class Metric(ABC):
+    def __init__(self, name, preprocessing=None):
+        self.name = name
+        self.preprocessing = PREPROCESSING[preprocessing] if preprocessing is not None else None
+        self.cv = StratifiedKFold(shuffle=True, random_state=42)
+
     def __call__(self, estimator, dataset: Dataset):
 
-        pipeline = Pipeline([
-            ('scaler', StandardScaler()),
-            ('model', estimator)
-        ])
+        if self.preprocessing is not None:
+            pipeline = Pipeline([
+                ('scaler', self.preprocessing()),
+                ('model', estimator)
+            ])
+        else:
+            pipeline = estimator
 
         return cross_val_score(pipeline, dataset.features, dataset.targets,
-                               scoring=self.get_score).mean()
+                               scoring=self.get_score, cv=self.cv).mean()
+    
+    def log_params(self):
+        return {'name': self.name,
+                'preprocessing': self.preprocessing.__name__ if self.preprocessing else None}
 
     @abstractclassmethod
     def get_score(self, model, x, y):
@@ -32,26 +49,26 @@ class Metric(ABC):
 
 
 class Accuracy(Metric):
-    def __init__(self):
-        self.name = 'accuracy'
+    def __init__(self, preprocessing):
+        super().__init__('accuracy', preprocessing)
 
     def get_score(self, model, x, y):
         return accuracy_score(y, model.predict(x))
 
 
 class F1(Metric):
-    def __init__(self, average: Literal['binary', 'macro', 'micro']):
+    def __init__(self, prepressing, average: Literal['binary', 'macro', 'micro']):
+        super().__init__(f'f1-score ({self.average})', prepressing)
         self.average = average
-        self.name = f'f1-score ({self.average})'
     
     def get_score(self, model, x, y):
         return f1_score(y, model.predict(x), average=self.average)
 
 
 class RocAuc(Metric):
-    def __init__(self, average: Literal['binary', 'macro', 'micro']):
+    def __init__(self, preprocessing, average: Literal['binary', 'macro', 'micro']):
+        super().__init__(f'f1-score ({self.average})', preprocessing)
         self.average = average
-        self.name = f'roc-auc ({self.average})'
     
     def get_score(self, model, x, y):
         if self.average == 'binary':
@@ -62,7 +79,7 @@ class RocAuc(Metric):
 
 class MSE(Metric):
     def __init__(self):
-        self.name = 'mse'
+        super().__init__('mse')
         
     def get_score(self, model, x, y):
         return mean_squared_error(y, model.predict(x))
